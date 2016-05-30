87110aaa52f32400d83ab41f2ccd7ffa89fc6023
David ODey <dodey@oreillyauto.com>
2016-05-30 13:29:15 -0700

    changed config

---
+++ _tasks/copy_site.js
@@ -0,0 +1,8 @@
+// Load plugins
+var gulp = require('gulp');
+
+// End of day tasks
+gulp.task('copy-site', function() {
+	gulp.src("_site/**")
+		.pipe(gulp.dest('/volumes/WWW/html/personal-site'));
+});
